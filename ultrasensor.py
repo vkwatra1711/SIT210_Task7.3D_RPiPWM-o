3f21cc4dba24d167269cf550662a06194f843389
vkwatra1711 <52880809+vkwatra1711@users.noreply.github.com>
2020-05-24 14:47:18 +1000

    Update ultrasensor.py

--- ultrasensor.py
+++ ultrasensor.py
@@ -4,7 +4,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 TRIG = 18
 ECHO = 24
-buzzer = 3
+buzzer = 4
 led = 2
 
 print(" Distance Measurement in progress")
@@ -14,7 +14,8 @@ GPIO.setup(ECHO,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 GPIO.setup(buzzer,GPIO.OUT)
 
-
+p = GPIO.PWM(2,20)
+q = GPIO.PWM(4,20)
 
 while True:
     GPIO.output(TRIG, False)
@@ -35,15 +36,20 @@ while True:
 
     distance = pulse_duration*11150
     distance = round(distance,2)
-
+    p.start(0)
+    q.start(0)
+    if 20 < distance < 100:
+        p.ChangeDutyCycle(2.0)
+	    q.ChangeDutyCycle(2.0)
+       
+        
     if distance < 20:
-        GPIO.output(buzzer, GPIO.HIGH)
-        GPIO.output(led, GPIO.LOW)
-        print("BUZZER ON/LED OFF")
-    else:
-        print("BUZZER OFF/LED ON")
-        GPIO.output(buzzer, GPIO.LOW)
-        GPIO.output(led, GPIO.HIGH)
+        p.ChangeDutyCycle(100.0)
+        q.ChangeDutyCycle(80.0)
+      
+    if distance > 100: 
+	p.stop()
+	q.stop()
 
 
 
